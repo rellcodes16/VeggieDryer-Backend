6cca70d9781d3b5b868c4977790230a0b0739cc6
Orji Laurel <125953264+rellcodes16@users.noreply.github.com>
2026-05-25 14:49:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://veggiedryer-frontend.onrender.com/",
     methods: ["GET", "POST"],
   },
 });
